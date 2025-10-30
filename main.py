b5e70581afff1f607c97069c9183caa9480758df
Peyotus <stcandrew@gmail.com>
2025-10-29 17:05:53 -0700

    added books and input

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-path_to_file = "books/frankenstein.txt"
-
-from stats import get_num_words
+import sys
+from stats import get_num_words, get_char_count, print_sort_dics
 
 def main():
-    get_num_words(path_to_file)
+    try:
+        path_to_file = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}...")
+        print("----------- Word Count ----------")
+        print(f"Found {get_num_words(path_to_file)} total words")
+        print("--------- Character Count -------")
+        print_sort_dics(get_char_count(path_to_file))
+        print("============= END ===============")
+    except:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    return
+
 main()
 
 
--- stats.py
+++ stats.py
@@ -1,12 +1,42 @@
 def get_num_words(path):
     with open(path) as f:
         file_contents = f.read()
-        word_list = []
         word_list = file_contents.split()
         count = 0
         for word in word_list:
             count += 1
-        print(f"Found {count} total words")
+    return count
+
+def get_char_count(path):
+    with open(path) as f:
+        file_contents = f.read()
+        word_list = file_contents.split()
+        char_dic = dict()
+        for word in range (0, len(word_list)):
+            letters = list(word_list[word])
+            for l in letters:
+                lwr = l.lower()
+                if lwr in char_dic:
+                    char_dic[lwr] += 1
+                else:
+                    char_dic[lwr] = 1
+    return char_dic
+
+def sort_on(items):
+    return items["num"]
+
+
+def print_sort_dics(unsorted_dic):
+    new_list = []
+    for key in unsorted_dic:
+        if key.isalpha():
+            char = key
+            num = unsorted_dic[key]
+            dic = {"char": char, "num": num}
+            new_list.append(dic)
+    new_list = sorted(new_list, reverse=True, key=sort_on)
+    for list_item in new_list:
+        print(f"{list_item["char"]}: {list_item["num"]}")
     return
 
 
